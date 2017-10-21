5c4a5b51fcbb64991753314656b9b69b2d0766f2
Cesar Toruño <31457738+cesartoruo15@users.noreply.github.com>
2017-10-20 20:31:22 -0600

    6
    nuevos procedures tbl empleados,admin,persona y tareas

--- app_bd/procedimientos.sql
+++ app_bd/procedimientos.sql
@@ -15,7 +15,7 @@ DELIMITER |
 		personaTipoRol enum('Administrador','Usuarios','Empleado'),
 		personaCodigoAcceso int(11))
 	BEGIN		
-		INSERT INTO tbl_persona(cedula, nombre, apellido, apellido2,
+		INSERT INTO tbl_persona(cedulaPersona, nombre, apellido, apellido2,
 								telefono, correo, fechaNacimiento, direccion, codigoAcceso, 
 								tipoRol, estado)
 		VALUES(personaCedula, personaNombre, personaApellido , personaApellido2,personaTelefono,
@@ -42,7 +42,7 @@ DELIMITER |
 		UPDATE tbl_persona set nombre = ApersonaNombre, apellido = ApersonaApellido, apellido2 = ApersonaApellido2,
 		telefono = ApersonaTelefono, correo = ApersonaCorreo, fechaNacimiento = ApersonaFechaNacimiento,
 		direccion = ApersonaDireccion, codigoAcceso = ApersonaCodigoAcceso, tipoRol = ApersonaTipoRol
-		 WHERE tbl_persona.cedula = ApersonaCedula;
+		 WHERE tbl_persona.cedulaPersona = ApersonaCedula;
 	END |
 DELIMITER ;
 ---------------------------------------------------------
@@ -51,18 +51,149 @@ DELIMITER |
 	CREATE PROCEDURE inactivarPersona(
     	personaCedula int(11))
 	BEGIN		
-		UPDATE tbl_persona set personaEstado = 'I' WHERE tbl_persona.personaCedula = personaCedula;
+		UPDATE tbl_persona set estado = 'I' WHERE tbl_persona.cedulaPersona = personaCedula;
 	END |
 DELIMITER ;
 ---------------------------------------------------------------
 DROP PROCEDURE if exists personasXcedula;
 DELIMITER |
 	CREATE PROCEDURE personasXcedula(
-		personaCedula int(11))
+		personaCedula int(11),
+		personaNombre varchar(45),
+		personaApellido varchar(45),
+		personaApellido2 varchar(45))
 	BEGIN		
-		SELECT personaCedula,personaNombre, personaApellido, personaApellido2
-        FROM tblestudiantes
-        WHERE estudianteEstado='A' AND tblestudiantes.estudianteCedula = estudianteCedula group by personaCedula;
+		SELECT *
+        FROM tbl_persona 
+        WHERE estado='A' AND tbl_persona.cedulaPersona = personaCedula group by personaCedula;
 	
     END |
 DELIMITER ;
+-- --------------------------------------------
+-- Procedimientos almacenados para Empleado
+-- --------------------------------------------
+
+DROP PROCEDURE if exists insertarEmpleado;
+DELIMITER |
+	CREATE PROCEDURE insertarEmpleado(
+		empleadocedulaPersona int(11),
+		empleadoPuesto varchar(45),
+		empleadoSalario varchar(45),
+		empleadoFechaIngreso date,
+		empleadoHorario varchar(45))
+	BEGIN		
+		INSERT INTO tbl_empleado(cedulaPersona,puesto,salario,fechaIngreso,horario,estado)
+		VALUES(empleadocedulaPersona,empleadoPuesto,empleadoSalario,empleadoFechaIngreso,empleadoHorario,'A');
+	END |
+DELIMITER ;
+------------------------------------------------------
+DROP PROCEDURE if exists modificarEmpleado;
+DELIMITER |
+	CREATE PROCEDURE modificarEmpleado(
+	    actualiza_empleadocedulaPersona int(11),
+		actualiza_empleadoPuesto varchar(45),
+		actualiza_empleadoSalario varchar(45),
+		actualiza_empleadoFechaIngreso date,
+		actualiza_empleadoHorario varchar(45))
+	BEGIN		
+		UPDATE tbl_empleado set puesto = actualiza_empleadoPuesto, salario = actualiza_empleadoSalario, fechaIngreso = actualiza_empleadoFechaIngreso,
+		horario = actualiza_empleadoHorario
+		 WHERE tbl_empleado.cedulaPersona = actualiza_empleadocedulaPersona;
+	END |
+DELIMITER ;
+---------------------------------------------------------
+DROP PROCEDURE if exists consultarEmpleado;
+DELIMITER |
+	CREATE PROCEDURE consultarEmpleado()
+  
+	BEGIN		
+		SELECT tbl_persona.cedulaPersona , tbl_persona.nombre , tbl_persona.apellido , tbl_persona.apellido2 , puesto, salario
+		FROM tbl_persona inner join tbl_empleado on tbl_persona.cedulaPersona = tbl_empleado.cedulaPersona 
+		WHERE tbl_empleado.estado = 'A' AND tbl_persona.cedulaPersona = tbl_empleado.cedulaPersona GROUP BY tbl_persona.cedulaPersona;
+	END |
+DELIMITER ;
+---------------------------------------------------------
+DROP PROCEDURE if exists inactivarEmpleado;
+DELIMITER |
+	CREATE PROCEDURE inactivarEmpleado(
+    	empleadocedulaPersona int(11))
+	BEGIN		
+		UPDATE tbl_empleado set estado = 'I' WHERE tbl_empleado.cedulaPersona = empleadocedulaPersona;
+	END |
+DELIMITER ;
+-- --------------------------------------------
+-- Procedimientos almacenados para Tareas
+-- --------------------------------------------
+DROP PROCEDURE if exists insertarTarea;
+DELIMITER |
+	CREATE PROCEDURE insertarTarea(
+		tareacedulaPersona int(11),
+		tareaDescripcion varchar(45),
+		tareaFecha date)
+	BEGIN		
+		INSERT INTO tbl_tareas(cedulaPersona,descripcion,fecha,estadoTarea,estado)
+		VALUES(tareacedulaPersona,tareaDescripcion,tareaFecha,'Incompleta','A');
+	END |
+DELIMITER ;
+------------------------------------------------------------
+DROP PROCEDURE if exists modificarTareas;
+DELIMITER |
+	CREATE PROCEDURE modificarTareas(
+	    actualiza_tareacedulaPersona int(11),
+		actualiza_tareaDescripcion varchar(45),
+		actualiza_tareaFecha date,
+		actualiza_tareaEstadoTarea enum('Completa','Incompleta'))
+	BEGIN		
+		UPDATE tbl_tareas set descripcion = actualiza_tareaDescripcion, fecha = actualiza_tareaFecha, estadoTarea = actualiza_tareaEstadoTarea
+		 WHERE tbl_tareas.cedulaPersona = actualiza_tareacedulaPersona;
+	END |
+DELIMITER ;
+---------------------------------------------------------------
+DROP PROCEDURE if exists inactivarTareas;
+DELIMITER |
+	CREATE PROCEDURE inactivarTareas(
+    	tareacedulaPersona int(11))
+	BEGIN		
+		UPDATE tbl_tareas set estado = 'I' WHERE tbl_tareas.cedulaPersona = tareacedulaPersona;
+	END |
+DELIMITER ;
+---------------------------------------------------------------------
+DROP PROCEDURE if exists consultarTareas;
+DELIMITER |
+	CREATE PROCEDURE consultarTareas()
+  
+	BEGIN		
+		SELECT cedulaPersona, descripcion, fecha, estadoTarea
+		FROM tbl_tareas;
+	END |
+DELIMITER ;
+---------------------------------------------------------------
+DROP PROCEDURE if exists consultaTareaXempleado;
+DELIMITER |
+	CREATE PROCEDURE consultaTareaXempleado(
+		tareacedulaPersona int (11))
+	BEGIN		
+		SELECT tbl_empleado.cedulaPersona, descripcion, estadoTarea, fecha
+        FROM tbl_tareas inner join tbl_empleado on tbl_tareas.cedulaPersona = tbl_empleado.cedulaPersona 
+        WHERE tbl_tareas.estado ='A' AND tbl_tareas.cedulaPersona = tareacedulaPersona GROUP BY fecha;
+    END |
+DELIMITER ;
+----------------------------------------------------------------
+DROP PROCEDURE if exists insertarAdministrador;
+DELIMITER |
+	CREATE PROCEDURE insertarAdministrador(
+		administradorCedulaPersona int(11),
+		administradorRolFamiliar varchar (45)
+		)
+	BEGIN		
+		INSERT INTO tbl_administrador(cedulaPersona,rolFamilia,estado)
+		VALUES(administradorCedulaPersona,administradorRolFamiliar,'A');
+	END |
+DELIMITER ;
+
+
+
+
+
+
+
