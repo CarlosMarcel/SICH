4cd7ea2c9f5053d418c6ebcd9c3d33476e0094a0
Cesar Toruño <31457738+cesartoruo15@users.noreply.github.com>
2017-11-08 05:15:13 -0600

    29
    luces

--- app_core/views/view_mantenimiento_luces.php
+++ app_core/views/view_mantenimiento_luces.php
@@ -9,11 +9,179 @@
 			<div class="row">
 				<form class="col s12">
 					<div class="row">
-						
+						<div class="row">
+							<div class="col s12 m12 l4 ">
+								<div class="card-panel">
+									<div class="wrap">
+										<div class="widget">
+											<div class="row">
+												<div class="col s12 m12 ">
+													<div class="card card-info">
+														<div class="card-image">
+															<div id="icon" class="center-align">
+																<i class="material-icons center-align" >wb_incandescent</i>
+															</div>
+														</div>
+														<div class="card-action">
+															<div class="card-title center-align" id="title_card">
+																<p >Sala</p>
+															</div><br> 
+															<!-- Switch -->
+															<div class="switch center-align">
+																<label>
+																	Off
+																	<input type="checkbox">
+																	<span class="lever"></span>
+																	On
+																</label>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col s12 m12 l4 ">
+								<div class="card-panel">
+									<div class="wrap">
+										<div class="widget">
+											<div class="row">
+												<div class="col s12 m12 ">
+													<div class="card card-info">
+														<div class="card-image">
+															<div id="icon" class="center-align">
+																<i class="material-icons center-align" >wb_incandescent</i>
+															</div>
+														</div>
+														<div class="card-action">
+															<div class="card-title center-align" id="title_card">
+																<p >Cocina</p>
+															</div><br> 
+															<!-- Switch -->
+															<div class="switch center-align">
+																<label>
+																	Off
+																	<input type="checkbox">
+																	<span class="lever"></span>
+																	On
+																</label>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col s12 m12 l4 ">
+								<div class="card-panel">
+									<div class="wrap">
+										<div class="widget">
+											<div class="row">
+												<div class="col s12 m12 ">
+													<div class="card card-info">
+														<div class="card-image">
+															<div id="icon" class="center-align">
+																<i class="material-icons center-align" >wb_incandescent</i>
+															</div>
+														</div>
+														<div class="card-action">
+															<div class="card-title center-align" id="title_card">
+																<p >Corredor</p>
+															</div><br> 
+															<!-- Switch -->
+															<div class="switch center-align">
+																<label>
+																	Off
+																	<input type="checkbox">
+																	<span class="lever"></span>
+																	On
+																</label>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col s12 m12 l4 ">
+								<div class="card-panel">
+									<div class="wrap">
+										<div class="widget">
+											<div class="row">
+												<div class="col s12 m12 ">
+													<div class="card card-info">
+														<div class="card-image">
+															<div id="icon" class="center-align">
+																<i class="material-icons center-align" >wb_incandescent</i>
+															</div>
+														</div>
+														<div class="card-action">
+															<div class="card-title center-align" id="title_card">
+																<p >Baño</p>
+															</div><br> 
+															<!-- Switch -->
+															<div class="switch center-align">
+																<label>
+																	Off
+																	<input type="checkbox">
+																	<span class="lever"></span>
+																	On
+																</label>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col s12 m12 l4 ">
+								<div class="card-panel">
+									<div class="wrap">
+										<div class="widget">
+											<div class="row">
+												<div class="col s12 m12 ">
+													<div class="card card-info">
+														<div class="card-image">
+															<div id="icon" class="center-align">
+																<i class="material-icons center-align" >wb_incandescent</i>
+															</div>
+														</div>
+														<div class="card-action">
+															<div class="card-title center-align" id="title_card">
+																<p >Cuarto</p>
+															</div><br> 
+															<!-- Switch -->
+															<div class="switch center-align">
+																<label>
+																	Off
+																	<input type="checkbox">
+																	<span class="lever"></span>
+																	On
+																</label>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 					<div class="row">
 						<button class="btn waves-effect waves-light" type="button" name="action">REGISTRAR<i class="material-icons right">send</i></button>
-                	</div>
+					</div>
 				</form>
 			</div>
 		</div>
